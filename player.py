e2a4c21da450c9fad84f6613493c3e71f0a23138
Fin <finfreezer@gmail.com>
2025-10-03 15:33:37 +0300

    Reduced fire-rate.

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.limiter = 0
     
     
     def triangle(self):
@@ -22,6 +23,8 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.limiter -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -33,7 +36,9 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if ( self.limiter <= 0 ):
+                self.limiter = PLAYER_SHOOT_COOLDOWN
+                self.shoot()
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
